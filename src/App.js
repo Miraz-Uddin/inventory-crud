3018aeecfd68601f2e0581cf156557cbcb589302
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-06-13 15:15:24 +0600

    Bootstrap 5 Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Navigator from "./components/Navigator";
 import Home from "./pages/Home";
+import Inventories from "./pages/Inventories";
+import ContactUs from "./pages/ContactUs";
 
 export default class App extends Component {
   render() {
@@ -11,6 +13,8 @@ export default class App extends Component {
         <Navigator />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/inventories" element={<Inventories />} />
+          <Route path="/contact" element={<ContactUs />} />
         </Routes>
       </Router>
     );
--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,9 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class Navigator extends Component {
   render() {
     return (
-      <div>Navigator</div>
-    )
+      <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container">
+            <NavLink className="navbar-brand" to="/"></NavLink>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav m-auto">
+                <li className="nav-item">
+                  <NavLink
+                    className={`nav-link ${(isActive) =>
+                      isActive ? "active" : ""}`}
+                    aria-current="page"
+                    to="/"
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    className={`nav-link ${(isActive) =>
+                      isActive ? "active" : ""}`}
+                    aria-current="page"
+                    to="/inventories"
+                  >
+                   Inventories
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    className={`nav-link ${(isActive) =>
+                      isActive ? "active" : ""}`}
+                    aria-current="page"
+                    to="/contact"
+                  >
+                    Contact Us
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </>
+    );
   }
 }
